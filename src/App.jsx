97d01a11b3114483b5056d97e7b436385bd69fda
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-19 11:12:46 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+    <h1>Full Stack Site</h1>
+    <p>A list of frameworks:</p>
+      <ul>
+        <li>data comes from backend</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
