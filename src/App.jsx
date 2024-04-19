8c8a32db864c7a511de1dec93441d11f04e62ceb
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-19 11:43:21 +0200

    shows frameworks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const backendUrl = "http://localhost:4588";
+
 function App() {
-  return (
-    <>
-    <h1>Full Stack Site</h1>
-    <p>A list of frameworks:</p>
-      <ul>
-        <li>data comes from backend</li>
-    </ul>
-    </>
-  )
+	const [frameworks, setFrameworks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${backendUrl}/frameworks`);
+			const _frameworks = await response.json();
+			setFrameworks(_frameworks);
+		})();
+	}, []);
+
+	return (
+		<>
+			<h1>Full Stack Site</h1>
+			<p>Here are {frameworks.length} frameworks:</p>
+			<ul>
+				{frameworks.map((framework, index) => {
+					return <li key={index}>{framework.title}</li>;
+				})}
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
